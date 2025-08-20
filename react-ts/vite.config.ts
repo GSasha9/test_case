914f93d448753d5f5ce52382a0b3b412de35c14f
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2025-08-20 08:52:43 +0300

    chore: setup deploy on gh-pages

--- react-ts/vite.config.ts
+++ react-ts/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/test_case/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
